049ae9f82a92ecd787552ef0b462acdbb175f412
peshopbs2 <peshopbs2@gmail.com>
2025-03-29 00:47:57 -0800

    add visits repository

---
+++ WebAPI/Repositories/Abstractions/IVisitRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebAPI.Data.Entities;
+
+namespace WebAPI.Repositories.Abstractions
+{
+    public interface IVisitRepository
+    {
+        Task CreateAsync(Visit visit);
+    }
+}
---
+++ WebAPI/Repositories/VisitRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebAPI.Data;
+using WebAPI.Data.Entities;
+using WebAPI.Repositories.Abstractions;
+
+namespace WebAPI.Repositories
+{
+    public class VisitRepository : IVisitRepository
+    {
+        private AppDbContext _context;
+        public VisitRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(Visit visit)
+        {
+            _context.Visit.Add(visit);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
